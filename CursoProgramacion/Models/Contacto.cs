5bd96b606e24be154eb1c1fe30aafba5a3ab2ee1
Daniel151505 <daniel150520011@gmail.com>
2020-12-19 12:41:14 -0500

    Añadiendo Display en los campos del formulario

--- CursoProgramacion/Models/Contacto.cs
+++ CursoProgramacion/Models/Contacto.cs
@@ -5,19 +5,25 @@ namespace CursoProgramacion.Models
 {
     public class Contacto
     { 
-
+         [Display (Name="Nombres")]
         public string Name { get; set; }
 
+         [Display (Name="Apellidos")]
         public string LastName { get; set; }
 
+         [Display (Name="Correos")]
         public string Email { get; set; }
 
+         [Display (Name="Telefono")]
         public int Number { get; set; }
-
+        
+         [Display (Name="Curso")]
         public int Course { get; set; }
 
+         [Display (Name="Titulo")]
         public string Title { get; set; }
 
+         [Display (Name="Mensaje")]
         public string Message { get; set; }
         
         public string Response {get;set;}
