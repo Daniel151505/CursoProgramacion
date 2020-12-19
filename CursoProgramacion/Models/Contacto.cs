238e568da741059d957b9639d13f27848d48be46
Daniel151505 <daniel150520011@gmail.com>
2020-12-19 15:11:45 -0500

    Agregación del metodo Id , el nombre de la tabla y las columnas

--- CursoProgramacion/Models/Contacto.cs
+++ CursoProgramacion/Models/Contacto.cs
@@ -1,32 +1,46 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CursoProgramacion.Models
 {
+    [Table("t-contacto")]
     public class Contacto
     { 
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("id")]
+        public int Id { get; set;}
+
+         [Column("name")]
          [Required(ErrorMessage="Debe escribir los nombres")]
          [Display (Name="Nombres")]
         public string Name { get; set; }
          
+         [Column("lastname")]
          [Required(ErrorMessage="Debe escribir los apellidos")]
          [Display (Name="Apellidos")]
         public string LastName { get; set; }
-
+         
+         [Column("email")]
          [Display (Name="Correos")]
         public string Email { get; set; }
 
          [DataType(DataType.PhoneNumber)]
+         [Column("number")]
          [Display (Name="Telefono")]
         public int Number { get; set; }
         
          [Display (Name="Curso")]
+         [Column("course")]
         public int Course { get; set; }
 
          [Display (Name="Titulo")]
+         [Column("title")]
         public string Title { get; set; }
 
          [Display (Name="Mensaje")]
+         [Column("message")]
         public string Message { get; set; }
         
         public string Response {get;set;}
