76c9a6cf83b14f57bf90af3f338daafe33e7682a
Daniel151505 <daniel150520011@gmail.com>
2020-12-18 22:03:23 -0500

    Creación de Calculadora Model

---
+++ CursoProgramacion/Models/Calculadora.cs
@@ -0,0 +1,21 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CursoProgramacion.Models
+{
+    public class Calculadora
+    {
+        public int Operador1 { get; set; }
+
+        public int Operador2 { get; set; }
+
+        public string Accion { get; set; }
+        
+        public int Respuesta { get; set; }
+
+
+    
+
+
+    }
+}
