d9f8ebaec36d437f5e4628ff50c2852a1097b27f
Daniel151505 <daniel150520011@gmail.com>
2020-12-18 22:40:05 -0500

    Creación de Metodos

--- CursoProgramacion/Models/Contacto.cs
+++ CursoProgramacion/Models/Contacto.cs
@@ -1,10 +1,31 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace CursoProgramacion.Models
 {
     public class Contacto
-    {
-        public string RequestId { get; set; }
+    { 
+
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public int Number { get; set; }
+
+        public int Course { get; set; }
+
+        public string Title { get; set; }
+
+        public string Message { get; set; }
+        
+        public string Response {get;set;}
+
+
+         
 
     }
 }
+        
+        
