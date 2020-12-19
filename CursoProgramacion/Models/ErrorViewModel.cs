f71828a2bd3fa60a0e950edcb5a7449c7b4bf480
Daniel151505 <daniel150520011@gmail.com>
2020-12-18 21:53:44 -0500

    Creación de estructura MVC

---
+++ CursoProgramacion/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CursoProgramacion.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
