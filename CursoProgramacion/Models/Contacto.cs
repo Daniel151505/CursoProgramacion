0221c5a1b1fdb3c20c9e77f4676cbb66ac34c34d
Daniel151505 <daniel150520011@gmail.com>
2020-12-19 23:54:00 -0500

    Fix model Contacto

--- CursoProgramacion/Models/Contacto.cs
+++ CursoProgramacion/Models/Contacto.cs
@@ -4,51 +4,49 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CursoProgramacion.Models
 {
-    [Table("t-contacto")]
+    [Table("t_contacto")]
     public class Contacto
     { 
-
+    
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         [Column("id")]
         public int Id { get; set;}
 
-         [Column("name")]
-         [Required(ErrorMessage="Debe escribir los nombres")]
-         [Display (Name="Nombres")]
+        [Required(ErrorMessage="Debe escribir los nombres")]
+        [Column("name")]
+        [Display (Name="Nombre")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage="Debe escribir los apellidos")]
          
-         [Column("lastname")]
-         [Required(ErrorMessage="Debe escribir los apellidos")]
-         [Display (Name="Apellidos")]
+
+        [Display (Name="Apellidos")]
+        [Column("lastname")]
         public string LastName { get; set; }
-         
+
+        [Display (Name="Correo")]  
          [Column("email")]
-         [Display (Name="Correos")]
         public string Email { get; set; }
 
-         [DataType(DataType.PhoneNumber)]
-         [Column("number")]
-         [Display (Name="Telefono")]
+        [DataType(DataType.PhoneNumber)]
+        [Display (Name="Telefono")]
+        [Column("number")]
         public int Number { get; set; }
-        
-         [Display (Name="Curso")]
-         [Column("course")]
-        public int Course { get; set; }
 
-         [Display (Name="Titulo")]
+        [Display (Name="Curso")]
+        [Column("course")]
+        public string Course { get; set; }
+
+        [Display (Name="Titulo")]
          [Column("title")]
         public string Title { get; set; }
 
-         [Display (Name="Mensaje")]
+        [Display (Name="Mensaje")]
          [Column("message")]
         public string Message { get; set; }
-        
+
+        [NotMapped]
         public string Response {get;set;}
-
-
-         
 
     }
 }
-        
-        
